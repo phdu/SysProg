da97fdb06198bbc0d7412bf23c95f84976c63d6f
julianmenzler <julianmenzler@ubuntu>
2014-10-24 08:15:32 -0700

    Julian's Test Commit 1

--- SimpleCompiler/src/SimpleCompiler.cpp
+++ SimpleCompiler/src/SimpleCompiler.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "So it begins..." << endl; // prints !!!Hello World!!!
+	cout << "I fucking hate this system!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
