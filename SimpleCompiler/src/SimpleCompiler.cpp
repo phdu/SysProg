7226cea4a643df724c425522f2804555b38a8bce
sauce-code <mail@sauce-code.com>
2014-10-24 17:04:03 +0200

    erster upload. los gehts!

---
+++ SimpleCompiler/src/SimpleCompiler.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SimpleCompiler.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "So it begins..." << endl; // prints !!!Hello World!!!
+	return 0;
+}
